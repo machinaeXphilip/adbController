94159766b3d2260e233ecdfa4019da078f13a802
majin <majin@majins-MBP.fritz.box>
2018-03-07 17:52:44 +0100

    call in and call out basic functionality done. next: make properly importable and add sms bridge  via http

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 
 /*
+adbControler
+ccncsa 4.0 (by) Philip Steimel www.machinaesc.de
+
+node script using adb (android-debug-bridge) as a bridge for incoming android
+outgoing calls between a computer and an android phone.
+
+basic hack to make robocalls from a computer. don't be evil! use it for fun stuff
+like scavanger hunts!
+
 $ adb --version
 Android Debug Bridge version 1.0.39
 Version 0.0.1-4500957
 Installed as /usr/local/bin/adb
+
+TO DOs:
+- add cancelTimer for clearInterval in case of no answered or errors
+- solve problem of soundrouting (hardware? cable soildering?)
+- decide on how to deal with the IncomingCall listener (always open or only if
+not calling in)
+
 */
 
 const shell = require('shelljs');
@@ -11,26 +27,32 @@ const player = require('play-sound')(opts = {}); // get better sound lib with ev
 
 
 let response;
+let runningInterval;
 
-// Run external tool synchronously
-response =shell.exec('adb devices',{silent:true})
 
-if ( response.code !== 0) {
-  shell.echo('Error: adb command failed');
-  shell.exit(1);
-}
-else {
-  console.log(response.stdout.trim("/n"));
 
-  // now start listener for incoming calls
+  // Run external tool synchronously
+  response =shell.exec('adb devices',{silent:true})
 
-    //setInterval(checkCALLIN, 1500);
+  if ( response.code !== 0)
+  {
+    shell.echo('Error: adb command failed');
+    shell.exit(1);
+  }
+  else
+  {
+    console.log(response.stdout.trim("/n"));
 
-    setInterval(checkCALLOUT, 500);
-    // make a testcall:
-    makeCALLOUT("+4915735981516");
+      // now start listener for incoming calls
+      setInterval(checkCALLIN, 1500);
 
-}
+      // make a testcall:
+      makeCALLOUT("+4915735981516");
+
+  }
+
+
+
 
 
 function checkCALLIN()
@@ -38,7 +60,6 @@ function checkCALLIN()
   let CallIncomingNumber = shell.exec('adb shell dumpsys telephony.registry | grep CallIncomingNumber=',{silent:true})
 
   CallIncomingNumber = CallIncomingNumber.stdout.replace("mCallIncomingNumber=","").replace("\n","").substr(2);
-
 
 
   if (CallIncomingNumber != "")
@@ -51,11 +72,12 @@ function checkCALLIN()
 }
 
 let state = "";
-let state_old = "old"
+let state_old = "busy" // to hinder busy trigger at startup
 
 function checkCALLOUT()
 {
-  let CallState = shell.exec('adb logcat -d -e PhoneStatusListener -T 2000',{silent:true})
+
+  let CallState = shell.exec('adb logcat -d -e PhoneStatusListener -T 5000',{silent:true})
 
   //CallState = CallState.stdout;
 
@@ -68,7 +90,7 @@ function checkCALLOUT()
 
   if (latestCallState != "")
   {
-    console.log(latestCallState);
+    //console.log(latestCallState);
 
 
     if (latestCallState.indexOf("PhoneState = OFFHOOK") !== -1 && latestCallState.indexOf("CallState = DIALING") !== -1)
@@ -81,38 +103,43 @@ function checkCALLOUT()
       state = "ringing";
       //console.log("Ringing");
     }
-    else if (latestCallState.indexOf("PhoneState = IDLE") !== -1 && latestCallState.indexOf("CallState = IDLE") !== -1 /*&& state != "online"*/)
+    else if (latestCallState.indexOf("PhoneState = IDLE") !== -1 && latestCallState.indexOf("CallState = IDLE") !== -1)
     {
-      state = "busy";
+      if (state == "online")
+      {
+        state = "success";
+      }
+      else if (state != "success")
+      {
+        state = "busy";
+      }
       //console.log("busy"); // busy only true if not Active before
     }
 
     else if (latestCallState.indexOf("PhoneState = OFFHOOK") !== -1 && latestCallState.indexOf("CallState = ACTIVE") !== -1)
     {
-      if (CallState.indexOf("PhoneState = IDLE") !== -1 && CallState.indexOf("CallState = IDLE") !== -1)
-      {
-        state = "sucess";
-      }
-      else
-      {
         state = "online"
-      }
     }
 
     //console.log(state);
     //console.log(CallState);
 
     // HERE REACT TO CALLSTATES:
+    if (state == "busy" || state == "success")
+    {
+      console.log("Callout ended");
+      clearInterval(runningInterval);
+    }
 
   }
 
-/*
+
   if (state != state_old)
   {
     console.log(state);
     state_old = state;
   }
-*/
+
 
 }
 
@@ -134,8 +161,9 @@ function hang()
 function makeCALLOUT(number)
 {
 
+  runningInterval = setInterval(checkCALLOUT, 500); // check if answered, ringing, busy or success
+
   shell.exec('adb shell am start -a android.intent.action.CALL -d tel:'+number,{silent:true});
-  // problem: how to see if answered or hung?
 
 
 }
@@ -146,3 +174,14 @@ function playsound()
     if (err) throw err
   })
 }
+
+
+
+// export the module (to be done: get global vars in via this.)
+module.exports = {
+    makeCALLOUT: makeCALLOUT,
+    checkCALLOUT: checkCALLOUT,
+    checkCALLIN: checkCALLIN,
+    answer: answer,
+    hang: hang
+};
